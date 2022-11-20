0c3825558cf12666e4ad1b77ec3c17f0e66750f2
Raghav Kumar <thisisraghavkumar@gmail.com>
2022-11-21 01:52:36 +0530

    Added logos

--- frontend/src/components/Social/SocialBar.tsx
+++ frontend/src/components/Social/SocialBar.tsx
@@ -1,15 +1,25 @@
+import { GithubOutlined, LinkedinOutlined, MailOutlined, TwitterOutlined } from "@ant-design/icons";
+
 const SocialBar = () => {
     return (
         <div className="social-bar">
+            <a href="https://twitter.com/thenameisRaghav">
+                <TwitterOutlined />
+                Twitter
+            </a>
+            &nbsp;|&nbsp;
             <a href="mailto:thisisraghavkumar@gmail.com">
+                <MailOutlined />
                 Gmail
             </a>
             &nbsp;|&nbsp;
             <a href="https://www.linkedin.com/in/raghav-kumar-79b002135/">
+                <LinkedinOutlined />
                 LinkedIn
             </a>
             &nbsp;|&nbsp;
             <a href="https://github.com/thisisraghavkumar/">
+                <GithubOutlined />
                 Github
             </a>
         </div>
