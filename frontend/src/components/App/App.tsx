2ade5ae3bcc5347275c8b9cbe3edd31f3d3fa8f0
Raghav Kumar <thisisraghavkumar@gmail.com>
2022-11-21 02:03:56 +0530

    Removed unused import

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ProfilePhoto from '../../assets/images/ProfilePicture.svg';
 import SocialBar from '../Social/SocialBar';
